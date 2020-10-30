6965a09b6fcffd0169b0cfbedc4d0cb0f25f099c
Gerardo Garcia <darknes.gerardo@gmail.com>
2020-10-30 12:32:29 -0600

    empezando a generar la secuencia

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const celeste = document.getElementById("celeste")
+const violeta = document.getElementById("violete")
+const naranja = document.getElementById("naranja")
+const verde = document.getElementById("verde")
+const btnEmpezar = document.getElementById("btnEmpezar")
+
+class  Juego {
+    constructor(){
+        this.inicializar()
+    }
+    inicializar(){
+        btnEmpezar.classList.add("hide")
+    }
+
+    generarSecuencia(){
+        this.secuencia = new Array(10).fill().map( n => Math.floor(Math.random() * 4))
+    }
+}
+
+
 function empezarjuego(){
-    alert("el juego va empezar")
+window.juego = new Juego ()
 }
